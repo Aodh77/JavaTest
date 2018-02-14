fb2f6f31a0967803e6a63f245f58ffb89cff0461
Aodh Oleary <aodholeary@des_laptop.ict.ad.dit.ie>
2018-02-14 16:57:51 +0000

    three

--- ie/dit/Tune.java
+++ ie/dit/Tune.java
@@ -1,7 +1,39 @@
-package ie.dit
+package ie.dit;
 
 
 public class Tune
 {
-	
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public String getAltTitle()
+	{
+		return altTitle;
+	}
+
+	public String getNotation()
+	{
+		return notation;
+	}
+
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+
+		sb.append(x + "," + title + "," + altTitle);
+
+		return sb .toString();
+	}
 }
--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,8 +1,45 @@
 package ie.dit;
 
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class TuneBook
 {
+	ArrayList<Tune> tunes = new ArrayList<Tune>();
+
+	public TuneBook(String n)
+	{
+		n = "abc.txt"
+		BufferedReader inputStream = null;
+		try {
+				inputStream = new BufferedReader(new FileReader(n));
+				String l;
+				while ((l = inputStream.readLine()) != null) {
+					tunes.add(l);
+				}
+			}
+			catch (IOException e)
+	        {
+	        e.printStackTrace();
+	        }
+	        finally
+	        {
+	        	if (inputStream != null){
+	        		try
+	        		{
+	        			inputStream.close();
+	        		}
+	        		catch(Exception e)
+	        		{
+	        			e.printStackTrace();
+	        		}
+	        	}
+	        }
+
+	}
 	
+
+
 }
